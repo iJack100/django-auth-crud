21de867e727064b4aa02a23b84fd55bc4e44cb92
iJack100 <jhoancevallosvillavicencio@gmail.com>
2026-07-02 00:17:29 -0500

    first commit

---
+++ Employee/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Tasks
+# Register your models here.
+class TaskAdmin (admin.ModelAdmin):
+    readonly_fields=('created',)
+admin.site.register(Tasks, TaskAdmin)
